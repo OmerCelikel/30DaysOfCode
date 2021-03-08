5a66f1a37fa9f4c9cd41e711ba4dd28cb0a3dbb7
Ömer <44753206+OmerCelikel@users.noreply.github.com>
2021-03-08 13:56:22 +0300

    Add files via upload

---
+++ 0 - Hello,World.py
@@ -0,0 +1,14 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+"""
+input :  Welcome to 30 Days of Code!
+output : Hello, World. 
+         Welcome to 30 Days of Code!
+"""
+# Print a string literal saying "Hello, World." to stdout.
+
+print('Hello, World.')
+print(input_string)
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
